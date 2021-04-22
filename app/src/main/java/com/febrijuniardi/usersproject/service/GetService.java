5c93b807146c06d35d39cb62842c409524720c00
febrijuniardi <febrijuniardi126@gmail.com>
2021-04-22 12:16:17 +0700

    UTS182101950

---
+++ app/src/main/java/com/febrijuniardi/usersproject/service/GetService.java
@@ -0,0 +1,13 @@
+package com.febrijuniardi.usersproject.service;
+
+import com.febrijuniardi.usersproject.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetService {
+    @GET("users")
+    Call<List<User>> getUsersList();
+}
